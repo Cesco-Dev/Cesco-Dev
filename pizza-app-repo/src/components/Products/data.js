e68b1b0693929b7cda033b5c36a091620b8db567
Cesco-Dev <90700681+Cesco-Dev@users.noreply.github.com>
2022-07-15 11:37:45 +0100

    Add files via upload

---
+++ pizza-app-repo/src/components/Products/data.js
@@ -0,0 +1,60 @@
+import product1 from '../images/Supreme.jpg'
+import product2 from '../images/Hawaiian.jpg'
+import product3 from '../images/Pizzavegi.jpg'
+import sweet1 from '../images/Donuts.jpg'
+import sweet2 from '../images/Doughnuts.jpg'
+import sweet3 from '../images/Icecream.jpg'
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '6,500',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Pizza',
+        name: 'Hawaiian Paradise',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '5,500',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'Pizza',
+        name: 'Veggie Overload',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '4,500',
+        button: 'Add to Cart'
+    }
+];
+
+export const productDataTwo = [
+    {
+        img: sweet2,
+        alt: 'Donuts',
+        name: 'Doughlicious',
+        desc: 'Belgian vanilla glazed donuts covered in icing with sprinkles on top',
+        price: '10,000',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet3,
+        alt: 'Ice Cream',
+        name: 'Caramel Wonder',
+        desc: 'Vanilla ice cream covered with caramel and chocolate glaze topped with coco stick',
+        price: '8,500',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet1,
+        alt: 'Brownie',
+        name: 'Brownie Bunch',
+        desc: 'Double fudge brownie squares topped with white chocolate pieces and nacadamia nuts',
+        price: '11,500',
+        button: 'Add to Cart'
+    }
+];
